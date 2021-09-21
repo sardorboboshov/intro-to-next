573c604287800617759b04b15989065099cc1ada
sardor boboshov <s.boboshov@student.inha.uz>
2021-09-22 04:01:21 +0500

    .

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 //.com/news
 
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <a href="/news/nextjs">NextJS Is A Great Framework</a>
+        </li>
+        <li>Somthing Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
